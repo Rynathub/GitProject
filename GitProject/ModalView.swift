28c3bec3b099776b70fe636195d32bb815de1e67
ShakaLap <shakirovrin228@gmail.com>
2025-02-04 00:57:18 +0200

    Main view has been embedded in NavigationView Has been add ModalView

---
+++ GitProject/ModalView.swift
@@ -0,0 +1,18 @@
+//
+//  ModalView.swift
+//  GitProject
+//
+//  Created by Rynat Shakirov on 04.02.2025.
+//
+
+import SwiftUI
+
+struct ModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ModalView()
+}
