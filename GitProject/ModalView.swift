bfb70b3a4869bb19bdc21208cd7acc30176fab59
ShakaLap <shakirovrin228@gmail.com>
2025-02-04 21:18:46 +0200

    Switched to navigation view

--- GitProject/ModalView.swift
+++ GitProject/ModalView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ModalView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("This is second View")
+        }
+        .navigationBarTitle("Second View",displayMode: .inline)
     }
 }
 
